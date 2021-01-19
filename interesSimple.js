a1eceaf1a45dc4b3aa2745fde7499d6a58f8a002
Simon Loyal <54008092+simonpedro33@users.noreply.github.com>
2021-01-19 19:50:26 -0300

    First Formula

---
+++ interesSimple.js
@@ -0,0 +1,10 @@
+
+// Es el pago del por el uso del dinero ajeno, o el rendimiento que se obtiene de algo.
+
+function interesSimple (valorPresente, valorFuturo){
+    I = valorPresente - valorFuturo
+    return I
+}
+
+//Ejemplo:
+
