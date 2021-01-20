6c794f4b9a04ab6a27987b9f7c8bf32e88c3b6b0
Simon Loyal <54008092+simonpedro33@users.noreply.github.com>
2021-01-19 21:04:14 -0300

    libreria files

--- capitulo1/ejercicios.js
+++ capitulo1/ejercicios.js
@@ -1,4 +1,13 @@
-const {interesSimple, tasaInteres, interesConTasaBase} = require('./interesSimple');
+const {interesSimple, tasaInteres, calcularMontoInicial} = require('./interesSimple');
 
+//#1 EJERCICIO 1.2
+//Un pequeño comerciante invierte 1000, después de un año recibe la cantidad de $1200.00 por su inversión.
 
-console.log('test', interesSimple(130, 100))
+//a) Calcular Interés
+console.log('El Interés', '$' + interesSimple(valorFuturo = 1200, valorPresente = 1000))
+
+//B) La Tasa de Interés
+console.log('Tasa de Interés', tasaInteres(interes = 200, capital = 1000, tiempo = 1) + '%')
+
+// Calcular el monto inicial
+console.log('calcular Monto Inicial', '$' + calcularMontoInicial(tasa = 20, capital = 1000, tiempo = 1))
--- capitulo1/interesSimple.js
+++ capitulo1/interesSimple.js
@@ -1,20 +1,20 @@
 // Interes Simple: Es el pago del por el uso del dinero ajeno, o el rendimiento que se obtiene de algo.
 const interesSimple = (valorFuturo, valorPresente) => {
   const interes = valorFuturo - valorPresente;
-  return interes;
+  return interes.toFixed(2);
 };
 
-//Tasa de Interés en el tiempo (se mide en años)
+//Tasa de Interés en el tiempo (la unidad de tiempo es 1 año)
 const tasaInteres = (interes, capital, tiempo) => {
     const tasa = interes / (capital * tiempo);
     const tasaPorcentaje = tasa * 100;
     return tasaPorcentaje;
   };
 
-// Interés con tasa Base
-  const interesConTasaBase = (tasa, capital, tiempo) => {
+// Interés con tasa Base a una unidad monetaria (Calcular Monto Inicial)
+  const calcularMontoInicial = (tasa, capital, tiempo) => {
     const interes = capital * (tasa / 100) * (tiempo);
     return interes.toFixed(2);
   };
 
-module.exports = { interesSimple, tasaInteres, interesConTasaBase }; 
+module.exports = { interesSimple, tasaInteres, calcularMontoInicial }; 
