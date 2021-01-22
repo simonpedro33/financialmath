b7aab63e7c7f63d0ac03ec34911326ae294bede0
Simon Loyal <54008092+simonpedro33@users.noreply.github.com>
2021-01-21 22:38:03 -0300

    ejercicios

--- capitulo1/ejercicios.js
+++ capitulo1/ejercicios.js
@@ -1,4 +1,4 @@
-const {interesSimple, tasaInteres, calcMontoInicial} = require('./interesSimple');
+const {interesSimple, tasaInteres, calcInteresPorPagar, calcTiempos} = require('./interesSimple');
 
 //#1 EJERCICIO 1.2
 //Un pequeño comerciante invierte 1000, después de un año recibe la cantidad de $1200.00 por su inversión.
@@ -9,5 +9,27 @@ console.log('El Interés', '$' + interesSimple(valorFuturo = 1200, valorPresente
 //B) La Tasa de Interés
 console.log('Tasa de Interés', tasaInteres(interes = 200, capital = 1000, tiempo = 1) + '%')
 
-// Calcular el monto inicial
-console.log('calcular Monto Inicial', '$' + calcMontoInicial(tasa = 20, capital = 1000, tiempo = 1))
+//C) Calcular el monto inicial
+console.log('calcular Interés por pagar', '$' + calcInteresPorPagar(tasa = 20, capital = 1000, tiempo = 1))
+
+
+//D) La Tasa de Interés automovil
+console.log('calcular Interés por pagar', '$' + calcInteresPorPagar(tasa = 36, capital = 98500, tiempo = 2))
+
+//1.5 Prestamos
+console.log('Tasa de Interés', tasaInteres(interes = 3000, capital = 15000, tiempo = 1) + '%')
+
+//1.6 Biciclistas
+console.log('Tasa de Interés', tasaInteres(interes = 660000, capital = 2500000, tiempo = 3) + '%')
+
+//1.8 banco
+console.log('calcular Interés por pagar', '$' + calcInteresPorPagar(tasa = 8, capital = 3800, tiempo = 0.25 ))
+
+//1.15 Ordinario vs Exacto
+console.log(calcTiempos(tasa = 25, capital = 18500, dias = 30 ))
+
+//1.16 Ordinario vs Exacto
+console.log(calcTiempos(tasa = 19.75, capital = 250000, dias = 60 ))
+
+//1.16 Ordinario vs Exacto
+console.log(calcTiempos(tasa = 29.52, capital = 25000, dias = 340 ))
--- capitulo1/interesSimple.js
+++ capitulo1/interesSimple.js
@@ -6,15 +6,33 @@ const interesSimple = (valorFuturo, valorPresente) => {
 
 //Tasa de Interés en el tiempo (la unidad de tiempo es 1 año)
 const tasaInteres = (interes, capital, tiempo) => {
-    const tasa = interes / (capital * tiempo);
-    const tasaPorcentaje = tasa * 100;
-    return tasaPorcentaje;
-  };
+  const tasa = interes / (capital * tiempo);
+  const tasaPorcentaje = tasa * 100;
+  return tasaPorcentaje.toFixed(1);
+};
 
 // Interés con tasa Base a una unidad monetaria (Calcular Monto Inicial)
-  const calcMontoInicial = (tasa, capital, tiempo) => {
-    const interes = capital * (tasa / 100) * (tiempo);
-    return interes.toFixed(2);
-  };
+const calcInteresPorPagar = (tasa, capital, tiempo) => {
+  const interes = capital * (tasa / 100) * tiempo;
+  return interes.toFixed(2);
+};
 
-module.exports = { interesSimple, tasaInteres, calcMontoInicial }; 
+// Relacion entre interés exacto 365 días y ordinario 360 días
+const calcTiempos = (tasa, capital, dias) => {
+  const tOrdinario = dias / 360;
+  const tExacto = dias / 365;
+  const ordinario = calcInteresPorPagar(tasa, capital, tOrdinario);
+  const exacto = calcInteresPorPagar(tasa, capital, tExacto);
+  const diferencia = ordinario - exacto;
+
+  return `El interés ordinario(comercial) es $${ordinario} y el interés exacto(real) es $${exacto} con una diferencia de $${diferencia.toFixed(
+    2
+  )} `;
+};
+
+module.exports = {
+  interesSimple,
+  tasaInteres,
+  calcInteresPorPagar,
+  calcTiempos,
+};
